ef2638831512005e615706fbfe3de8b9e1911f07
erliaananda <erliaananda11@gmail.com>
2023-02-20 10:13:47 +0700

    first commit

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Laravel 9 CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
